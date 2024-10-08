40816d867c8e1c5fc287561973d29a2a3b9ea3ca
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-10-08 14:07:30 +0200

    Aufgabe1

---
+++ src/main/java/de/lubowiecki/okt24/Aufgabe1.java
@@ -0,0 +1,11 @@
+package de.lubowiecki.okt24;
+
+public class Aufgabe1 {
+
+    /*
+    Schreibe eine Fahrzeugklasse deren Objekte über equals miteinander vergleichbar sind.
+    Beim gleich Inhalt der Objekte soll true und aufruf von hashCode gleicher Wert rauskommen.
+    */
+
+
+}
