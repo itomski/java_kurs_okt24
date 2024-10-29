1814f2d290b1bda267c93ba8dfd7574332d3970c
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-10-29 13:07:23 +0100

    Aufgabe

---
+++ src/main/java/de/lubowiecki/okt24/AufgabeExport.java
@@ -0,0 +1,12 @@
+package de.lubowiecki.okt24;
+
+public class AufgabeExport {
+
+    // Erweitere die Produktverwaltung um eine Import und Export Funktion
+    // Export kann in Excel oder JSON erfolgen
+    // Import aus Excel
+
+    // Lagere die Zugangsdaten für die Datenbank (die URL) in eine XML datei aus,
+    // die beim Start des Programms eigelesen wird
+
+}
